ebce77b0594af2672d324dca83869af220353cc7
Hasan Ali <hasan.myoffice1@gmail.com>
2026-08-05 12:59:12 +0600

    Added Theme Variables.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
   const navLinkClass = ({ isActive, isPending }) => {
@@ -11,12 +11,28 @@ const Header = () => {
   };
 
   return (
-    <div className='text-gray-800 flex gap-6 transition-all'>
-      <NavLink to='/' className={navLinkClass}>Home</NavLink>
-      <NavLink to='/services' className={navLinkClass}>Services</NavLink>
-      <NavLink to='/about' className={navLinkClass}>About</NavLink>
-      <NavLink to='/contact' className={navLinkClass}>Contact</NavLink>
-    </div>
+    <>
+        <header className='dark:bg-gray-800 bg-gray-100 py-3 px-5 lg:px-20 flex  justify-between'>
+            <Link to='/'><img src="/src/assets/Logo.png" alt="Logo" className='w-32 sm:w-48' alt='Logo'/></Link>
+
+            <div className='mt-2  transition-all hidden md:block'>
+                <nav className='flex align-bottom gap-7 dark:text-gray-200 text-gray-800'>
+                  <NavLink to='/' className={navLinkClass}>Home</NavLink>
+                  <NavLink to='/services' className={navLinkClass}>Services</NavLink>
+                  <NavLink to='/about' className={navLinkClass}>About</NavLink>
+                  <NavLink to='/contact' className={navLinkClass}>Contact</NavLink>
+                </nav>
+            </div>
+
+            <button className='button-primary hidden md:block'>
+                <Link to='/contact' >Free Quote</Link>
+            </button>
+
+            <button className='w-[40px] h-[40px] bg-primary-color hover:bg-secondary-color rounded-full flex absolute top-[400px] right-[3%]  z-[-1] mood-button hover:shadow-gray-700 shadow-lg shadow-gray-600 animate-bounce'>
+                <img src="/public/solar_moon-line-duotone.svg" alt="" className='m-auto' />
+            </button>
+        </header>
+    </>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,11 @@ const Home = () => {
     return (
         <div>
             <Header />
-            <h2 className='font-bold text-shadow-blue-700'>Home Page</h2>
+            <h2 className='font-bold text-hero-heading'>Home Page</h2>
+            <h2 className='font-bold text-section-heading'>Home Page</h2>
+            <h3 className='font-bold text-section-title'>Home Page</h3>
+            <h3 className='font-bold text-card-title'>Home Page</h3>
+            <h3 className='section-small-heading'>Home Page</h3>
         </div>
     );
 };
